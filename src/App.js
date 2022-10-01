2b079ee803bfc341c876554050e0ba43abdc183b
Srinivas Tamada <srinivas@9lessons.info>
2022-09-30 23:38:31 -0400

    React Post Process

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <h1>
+          <i class="bi bi-robot"></i> Welcome to React Build CSS
+        </h1>
+        <div>
+          <button class="btn btn-success" type="submit">
+            <i class="bi bi-bug-fill"></i> Test Button
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
