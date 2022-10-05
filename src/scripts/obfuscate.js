5ecd1e150d7f79e87578d39ccce9fbb21526c440
Srinivas Tamada <srinivas@9lessons.info>
2022-10-04 23:01:00 -0400

    changes

--- src/scripts/obfuscate.js
+++ src/scripts/obfuscate.js
@@ -1,32 +1,34 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const JavaScriptObfuscator = require('javascript-obfuscator');
 
 const settings = {
-    compact: true
-}
+  compact: true,
+};
 
 function obfuscateDir(dirPath) {
-    var dirents = fs.readdirSync(dirPath, { encoding: "utf8", withFileTypes: true });
-    for (let i = 0; i < dirents.length; i++) {
-        let dirent = dirents[i];
-        
-        if (dirent.isDirectory()){
-            obfuscateDir(path.join(dirPath, dirent.name));
-            continue;
-        }
-
-        if (path.extname(dirent.name) !== ".js") continue;
-
-        let filePath = path.join(dirPath, dirent.name);
-        let content = fs.readFileSync(filePath, { encoding: "utf8" });
-
-        let obfuscator = JavaScriptObfuscator.obfuscate(content, settings);
-        let obfuscatedCode = obfuscator.getObfuscatedCode();
-
-        fs.writeFileSync(filePath, obfuscatedCode, { encoding: "utf8", flag: "w+" });
-        console.log("🤖 🤖 🤖 Done!");
+  let dirents = fs.readdirSync(dirPath, {
+    encoding: 'utf8',
+    withFileTypes: true,
+  });
+  for (let i = 0; i < dirents.length; i++) {
+    let dirent = dirents[i];
+    if (dirent.isDirectory()) {
+      obfuscateDir(path.join(dirPath, dirent.name));
+      continue;
     }
+    if (path.extname(dirent.name) !== '.js') continue;
+    const filePath = path.join(dirPath, dirent.name);
+    const content = fs.readFileSync(filePath, { encoding: 'utf8' });
+    const obfuscator = JavaScriptObfuscator.obfuscate(content, settings);
+    const obfuscatedCode = obfuscator.getObfuscatedCode();
+
+    fs.writeFileSync(filePath, obfuscatedCode, {
+      encoding: 'utf8',
+      flag: 'w+',
+    });
+    console.log('🤖 🤖 🤖 Done!');
+  }
 }
 
-obfuscateDir(path.join(__dirname, "../../build"));
+obfuscateDir(path.join(__dirname, '../../build'));
